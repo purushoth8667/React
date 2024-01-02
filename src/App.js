1bb5a98625ae5fcf6f888e6c564e28b4cd9ad2d3
purushoth8667 <purushoth8667@gmail.com>
2024-01-02 13:08:57 +0530

    comment the app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import Adddata from "./Adddata";
+// import Adddata from "./Adddata";
 import Fetch from "./Fetch";
 import Addproduct from "./Addproduct";
 const App = () => {
   return <Router>
     <div>
       <Routes>
-      <Route path="/" element={<Adddata/>}/>
+      {/* <Route path="/" element={<Adddata/>}/> */}
       <Route path="/fetch" element={<Fetch/>}/>
       <Route path="/add" element={<Addproduct/>}/>
       </Routes>
